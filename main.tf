a0fb0a6f3ea7c9e91ba5608bcbe999f78d67ef92
Psaulnie <63807531+psaulnie@users.noreply.github.com>
2023-07-28 13:12:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,27 +14,27 @@ provider "aws" {
 }
 
 resource "aws_default_vpc" "default" {
-   tags = {
-     Name = "Default VPC"
-   }
+  tags = {
+    Name = "Default VPC"
+  }
 }
 
 resource "aws_default_security_group" "default" {
-   vpc_id      = "${aws_default_vpc.default.id}"
- ingress {
-     # TLS
-     from_port   = 22
-     to_port     = 22
-     protocol    = "tcp"
-     cidr_blocks     = ["0.0.0.0/0"]
-   }
- egress {
-     from_port       = 0
-     to_port         = 0
-     protocol        = "-1"
-     cidr_blocks     = ["0.0.0.0/0"]
-   }
- }
+  vpc_id = aws_default_vpc.default.id
+  ingress {
+    # TLS
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_instance" "app_server" {
   ami           = "ami-007ec828a062d87a5"
