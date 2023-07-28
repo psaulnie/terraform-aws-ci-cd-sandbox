9ea60510e2fb02b58afd4c100c1abf6f6dece6bc
Psaulnie <63807531+psaulnie@users.noreply.github.com>
2023-07-28 12:16:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,9 +13,33 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+resource "aws_default_vpc" "default" {
+   tags = {
+     Name = "Default VPC"
+   }
+}
+
+resource "aws_default_security_group" "default" {
+   vpc_id      = "${aws_default_vpc.default.id}"
+ ingress {
+     # TLS
+     from_port   = 22
+     to_port     = 22
+     protocol    = "tcp"
+     cidr_blocks     = ["0.0.0.0/0"]
+   }
+ egress {
+     from_port       = 0
+     to_port         = 0
+     protocol        = "-1"
+     cidr_blocks     = ["0.0.0.0/0"]
+   }
+ }
+
 resource "aws_instance" "app_server" {
   ami           = "ami-007ec828a062d87a5"
   instance_type = "t2.micro"
+  key_name      = "tf-key-pair"
 
   tags = {
     Name = "TerraformUbuntuInstance"
